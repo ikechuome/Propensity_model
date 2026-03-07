6d5c3a521d3a7b3f9fdc159a5a77f8b5b9cf4fa4
ikechuome <ikechuome@gmail.com>
2026-03-07 17:18:30 +0000

    initial commit 8

--- src/data_processing.py
+++ src/data_processing.py
@@ -5,21 +5,22 @@ import numpy as np
 import os
 
 # Base directory — always points to where data_processing.py lives
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, '..', 'data')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR,'data')
 
 def clean_data(df):
     # Drop irrelevant data
-    data = df.drop(columns=['UserID'])
+    df = df.drop(columns=['UserID'])
     
     # Save to data folder regardless of where function is called from
+    
     output_path = os.path.join(DATA_DIR, 'final_version.csv')
-    # data.to_csv(output_path, index=False)
-    data.to_csv('../data/final_version',index=False)
+ 
+    df.to_csv(output_path,index=False)
     # print(f'Clean training data saved to: {output_path}')
     print('Clean training data saved')
     
-    return data
+    return df
 
 def clean_data_test(df):
     # Drop irrelevant data
